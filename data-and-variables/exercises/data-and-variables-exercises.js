9ef77a5c1a16f841a04bbef6d4bb686bb6d6daf6
Telly Pippa <telly@Tellys-PC.local>
2024-03-25 15:37:41 -0500

    exercise

--- data-and-variables/exercises/data-and-variables-exercises.js
+++ data-and-variables/exercises/data-and-variables-exercises.js
@@ -9,3 +9,41 @@
 // Calculate a trip to the moon below
 
 // Print the results of the trip to the moon below
+
+let nameOfTheSpaceShuttle = ("Determination");
+
+let shuttleSpeed = (17500);
+
+let distanceToMars = (225000000);
+
+let distanceToTheMoon = (384400);
+
+let milesPerKilometer = (0.621);
+
+console.log(typeof nameOfTheSpaceShuttle);
+
+console.log(typeof shuttleSpeed)
+
+console.log(typeof distanceToMars)
+
+console.log(typeof distanceToTheMoon)
+
+console.log(typeof milesPerKilometer)
+
+let milesToMars = distanceToMars * milesPerKilometer;
+
+let hoursToMars = milesToMars / shuttleSpeed;
+
+let daysToMars = hoursToMars / 24;
+
+console.log(nameOfTheSpaceShuttle+"will take"+
+daysToMars+"days to reach Mars");
+
+let milesToMoon = distanceToTheMoon * milesPerKilometer;
+
+let hoursToTheMoon = milesToMoon / shuttleSpeed;
+
+let daysToTheMoon = hoursToTheMoon / 24;
+
+console.log(nameOfTheSpaceShuttle + "will take" + daysToTheMoon + "days to reach the Moon.");
+
