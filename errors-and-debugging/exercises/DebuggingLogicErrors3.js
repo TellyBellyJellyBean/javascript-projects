4317abb1b851001266ece727d091a46cde7a5566
Telly Pippa <telly@Tellys-PC.local>
2024-03-26 16:13:27 -0500

    lifting errors

--- errors-and-debugging/exercises/DebuggingLogicErrors3.js
+++ errors-and-debugging/exercises/DebuggingLogicErrors3.js
@@ -6,17 +6,17 @@
 // Is the program behaving as expected?
 
 let launchReady = false;
-// let fuelLevel = 17000;
+let fuelLevel = 17000;
 let crewStatus = true;
 let computerStatus = 'green';
 
-// if (fuelLevel >= 20000) {
-//    console.log('Fuel level cleared.');
-//    launchReady = true;
-// } else {
-//    console.log('WARNING: Insufficient fuel!');
-//    launchReady = false;
-// }
+if (fuelLevel >= 20000) {
+   console.log('Fuel level cleared.');
+   launchReady = true;
+} else {
+   console.log('WARNING: Insufficient fuel!');
+   launchReady = false;
+}
 
 if (crewStatus && computerStatus === 'green'){
    console.log('Crew & computer cleared.');
@@ -25,6 +25,8 @@ if (crewStatus && computerStatus === 'green'){
    console.log('WARNING: Crew or computer not ready!');
    launchReady = false;
 }
+
+console.log(launchReady);
 
 // if (launchReady) {
 //    console.log('10, 9, 8, 7, 6, 5, 4, 3, 2, 1...');
